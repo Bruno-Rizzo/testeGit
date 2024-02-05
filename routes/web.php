49d24a86714fe0cc23cbf52e796bdce7373c1292
Bruno Rizzo <bruno25.rs@gmail.com>
2024-02-05 09:59:52 -0300

    criacção da classe Teste

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/teste', [TesteController::class, 'index'])->name('teste.index');
 
 require __DIR__.'/auth.php';
